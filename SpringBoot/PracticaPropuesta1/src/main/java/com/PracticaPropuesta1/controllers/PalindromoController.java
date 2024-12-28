f427e92f9993ece8eb10b08beff6fac4a6456bcd
FerLeon2008 <fernando.lg130308@gmail.com>
2024-12-27 21:25:36 -0600

    Se realizó la documentación del la práctica

--- SpringBoot/PracticaPropuesta1/src/main/java/com/PracticaPropuesta1/controllers/PalindromoController.java
+++ SpringBoot/PracticaPropuesta1/src/main/java/com/PracticaPropuesta1/controllers/PalindromoController.java
@@ -3,10 +3,16 @@ package com.PracticaPropuesta1.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+/*
+    controlador par verificar palindromos
+ */
 @RestController
 public class PalindromoController {
-    
+    /**
+     * EndPoint para verificar si la palabra es un palindromo
+     * @param palabra (Palabra a verificar)
+     * @return Un mensaje indicando si la palabra es un palindromo o no
+     */
     @GetMapping("/validarPalindromo/{palabra}")
     public String Palindromo(@PathVariable String palabra){
         if(isPalindrome(palabra)){
@@ -15,11 +21,16 @@ public class PalindromoController {
             return "La palabra " + palabra + " NO es un palindromo";
         }
     }
-    
-    private boolean isPalindrome(String palindromo){
-        int length = palindromo.length();
+
+    /**
+     * Método para verificar si la palabra es un palindromo
+     * @param palabra (recibe la cadena de texto y la verifica)
+     * @return false, si no es palindromo, y true, si es palindromo
+     */
+    private boolean isPalindrome(String palabra){
+        int length = palabra.length();
         for (int i = 0; i < length/2; i++) {
-            if(palindromo.charAt(i) != palindromo.charAt(length-(1+i))){
+            if(palabra.charAt(i) != palabra.charAt(length-(1+i))){
                 return false;
             }
         }
