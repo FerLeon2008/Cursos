fb2bbbc8049c273797c1e535970fd4f1af53a401
FerLeon2008 <fernando.lg130308@gmail.com>
2024-12-27 18:46:47 -0600

    Se crea el curso de SpringBoot

---
+++ SpringBoot/curso-Spring-boot/src/main/java/curso_spring_boot/CursoSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package curso_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootAppApplication.class, args);
+	}
+
+}
