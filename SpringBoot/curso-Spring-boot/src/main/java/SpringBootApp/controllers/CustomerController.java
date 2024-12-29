bdd7c543ec60d8f225b2bed5cf2ffecac01ae576
FerLeon2008 <fernando.lg130308@gmail.com>
2024-12-29 14:46:43 -0600

    11ra clase del curso de SpringBoot, en esta se aprendió más acerca de los enrutamientos de URL

--- SpringBoot/curso-Spring-boot/src/main/java/SpringBootApp/controllers/CustomerController.java
+++ SpringBoot/curso-Spring-boot/src/main/java/SpringBootApp/controllers/CustomerController.java
@@ -2,6 +2,7 @@ package SpringBootApp.controllers;
 
 import SpringBootApp.domain.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -23,4 +24,13 @@ public class CustomerController {
         return customers;
     }
 
+    @GetMapping("/userData/{userName}")
+    public Customer getUser(@PathVariable String userName){
+        for (Customer u : customers){
+            if(u.getUserName().equalsIgnoreCase(userName)){
+                return u;
+            }
+        }
+        return null;
+    }
 }
