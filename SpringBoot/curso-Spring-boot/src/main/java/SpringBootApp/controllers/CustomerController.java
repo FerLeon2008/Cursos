8be703732dd44e82d3735f2951cbac44d8c45370
FerLeon2008 <fernando.lg130308@gmail.com>
2024-12-29 14:31:23 -0600

    10ma clase del curso de SpringBoot, se creó un microservicio para obtener los usuarios registrados en una API

---
+++ SpringBoot/curso-Spring-boot/src/main/java/SpringBootApp/controllers/CustomerController.java
@@ -0,0 +1,26 @@
+package SpringBootApp.controllers;
+
+import SpringBootApp.domain.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    private List<Customer> customers = new ArrayList<>(Arrays.asList(
+        new Customer(123, "Fernando León", "TheLord", "PASSWORD"),
+        new Customer( 60065,"Giovanni Ramírez", "Putita23", "SoyGAY"),
+        new Customer( 1304,"Erick Romero", "ErickByJazmine", "JazmineLover2025"),
+        new Customer( 1403, "María Rodríguez", "PrideLesbian2025", "OnlyLesbians")
+    ));
+
+    @GetMapping("/userData")
+    public List<Customer> getCustomers(){
+        return customers;
+    }
+
+}
