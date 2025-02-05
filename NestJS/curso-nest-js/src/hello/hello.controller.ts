914a7790d30ebbbcdcb1c46fb9a476769d3f41fe
FerLeon2008 <fernando.lg130308@gmail.com>
2025-02-04 21:16:25 -0600

    9na sesión del curso de NestJS. Se aprende a como manejar HTTP status, para todo el manejo de solicitudes HTTP

--- NestJS/curso-nest-js/src/hello/hello.controller.ts
+++ NestJS/curso-nest-js/src/hello/hello.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 
 @Controller()
 export class HelloController {
@@ -6,5 +6,23 @@ export class HelloController {
     @Get('/')
     index() {
         return 'Home page';
+    }
+
+    @Get('new')
+    @HttpCode(201)
+    somethingNew() {
+        return 'Something new';
+    }
+
+    @Get('notfound')
+    @HttpCode(404)
+    notFoundPage() {
+        return '404 not found ';
+    }
+
+    @Get('error')
+    @HttpCode(500)
+    errorPage() {
+        return 'Error Route';
     }
 }
