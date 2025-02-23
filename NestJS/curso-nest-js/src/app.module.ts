c9cfa5271f495e4b5e3a27f4ac80702174432296
FerLeon2008 <fernando.lg130308@gmail.com>
2025-02-22 22:19:12 -0600

    13ra sesión del curso de NestJS. Se aprendió a crear los resources,es una forma de crear toda la API REST de forma más rápida y sencilla

--- NestJS/curso-nest-js/src/app.module.ts
+++ NestJS/curso-nest-js/src/app.module.ts
@@ -4,9 +4,12 @@ import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './projects/projects.module';
 import { UsersModule } from './users/users.module';
 import { HelloController } from './hello/hello.controller';
+import { PaymentsResolver } from './payments/payments.resolver';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [TasksModule, AuthModule, ProjectsModule, UsersModule],
+  imports: [TasksModule, AuthModule, ProjectsModule, UsersModule, PaymentsModule],
   controllers: [HelloController],
+  providers: [PaymentsResolver],
 })
 export class AppModule {}
