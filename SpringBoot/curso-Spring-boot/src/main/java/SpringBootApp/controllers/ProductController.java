8b9366bd11ef8760591e547e1b9a8c38dac02be0
FerLeon2008 <fernando.lg130308@gmail.com>
2025-03-09 05:55:02 -0600

    24va clase del curso de SpringBoot. Se aprendió como inyectar una dependencia y el principio de inversón de control (IoC, junto con los beans y como crear uno para instanciarlo en otro lugar

--- SpringBoot/curso-Spring-boot/src/main/java/SpringBootApp/controllers/ProductController.java
+++ SpringBoot/curso-Spring-boot/src/main/java/SpringBootApp/controllers/ProductController.java
@@ -3,6 +3,7 @@ package SpringBootApp.controllers;
 import SpringBootApp.domain.Product;
 import SpringBootApp.service.ProductService;
 import SpringBootApp.service.ProductsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,9 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
-    ProductService productsService = new ProductsServiceImpl();
+    // Instancia de clase
+    @Autowired
+    private ProductService productsService;
 
     @GetMapping
     public ResponseEntity<?> getProducts() {
--- SpringBoot/curso-Spring-boot/src/main/java/SpringBootApp/service/ProductsServiceImpl.java
+++ SpringBoot/curso-Spring-boot/src/main/java/SpringBootApp/service/ProductsServiceImpl.java
@@ -1,11 +1,13 @@
 package SpringBootApp.service;
 
 import SpringBootApp.domain.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class ProductsServiceImpl implements ProductService
 {
 
