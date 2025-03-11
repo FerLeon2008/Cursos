ff55600784b0786257c06033a5cd2b81f396d16d
FerLeon2008 <fernando.lg130308@gmail.com>
2025-03-10 21:33:37 -0600

    ayudando a ian

--- Hibernate/curso-hibernate/src/main/java/org/example/entities/Employee.java
+++ Hibernate/curso-hibernate/src/main/java/org/example/entities/Employee.java
@@ -1,4 +1,125 @@
 package org.example.entities;
 
-public class Employee {
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name= "ob_employees") //opcional
+public class Employee implements Serializable {
+
+    //atributos(Cada atributo representa una columna)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String firstName;
+
+    @Column(unique = true)
+    private String email;
+
+    private Integer age;
+
+    private Double salary;
+
+    private Boolean married;
+
+    private LocalDate birthDate;
+
+    private LocalDateTime registerDate;
+
+    //constructors
+    public Employee() {}
+
+    public Employee(Long id, String firstName, String email, Integer age, Double salary, Boolean married,
+                    LocalDate birthDate, LocalDateTime registerDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.email = email;
+        this.age = age;
+        this.salary = salary;
+        this.married = married;
+        this.birthDate = birthDate;
+        this.registerDate = registerDate;
+    }
+
+    //getter and setter
+    public LocalDateTime getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(LocalDateTime registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Boolean getMarried() {
+        return married;
+    }
+
+    public void setMarried(Boolean married) {
+        this.married = married;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", married=" + married +
+                ", birthDate=" + birthDate +
+                ", registerDate=" + registerDate +
+                '}';
+    }
 }
--- Hibernate/curso-hibernate/src/test/java/com/example/entities/EmployeeTest.java
+++ Hibernate/curso-hibernate/src/test/java/com/example/entities/EmployeeTest.java
@@ -1,4 +1,15 @@
-package com.example;
+package com.example.entities;
+
+import org.example.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.Test;
 
 public class EmployeeTest {
+
+    @Test
+    void createTableTest(){
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+    }
 }
